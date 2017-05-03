fee25c232f2eab0cf6218156c63f42bc13d70ef7
jeroenpeters <jeroenpeters@localhost>
2017-05-04 01:30:20 +0200

    Progress

--- services/buienradar.py
+++ services/buienradar.py
@@ -2,17 +2,15 @@
 import urllib
 import xml.etree.cElementTree as etree
 
-from config import settings
 
-
-def get_xml_location():
-    """ Return XML Location (HTTP address) for Buienradar """
-    return settings.BUIENRADAR_XML_URL
+def get_xml_base_location():
+    """ Return HTTP XML Location for Buienradar """
+    return "http://xml.buienradar.nl"
 
 
 def get_weather_from_station(station_id):
     """ Gather weather information based on a weather station ID """
-    initial_xml = urllib.urlopen(get_xml_location()).read()
+    initial_xml = urllib.urlopen(get_xml_base_location()).read()
     xml_rootelement = etree.fromstring(initial_xml)
 
     weather_info = {}
@@ -30,3 +28,17 @@ def get_weather_from_station(station_id):
         weather_info['zin'] = '\n'.join(desc_words)
 
     return weather_info
+
+
+def get_station_temperature(station_id):
+    """ Gather weather information based on a weather station ID """
+    initial_xml = urllib.urlopen(get_xml_base_location()).read()
+    xml_rootelement = etree.fromstring(initial_xml)
+
+    for weather_station in xml_rootelement[0][7][0]:
+        if weather_station.attrib['id'] == station_id:
+            for item in weather_station:
+                if item.tag == 'temperatuurGC':  # temperatuur10cm
+                    return float(item.text)
+
+    return 1000
